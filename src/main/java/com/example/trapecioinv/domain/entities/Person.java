ec16573f59d347aa0ab70de4882412f00ce71d0a
BurgosManuel <trapecioinv@gmail.com>
2024-07-27 15:37:03 -0300

    ADD: Repository

--- src/main/java/com/example/trapecioinv/domain/entities/Person.java
+++ src/main/java/com/example/trapecioinv/domain/entities/Person.java
@@ -1,9 +1,16 @@
 package com.example.trapecioinv.domain.entities;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 
 @Data
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String name;
     private String lastName;
     private String email;
