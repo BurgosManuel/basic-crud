ee6e69aee60fcac278519fda660034afe0e4e04c
BurgosManuel <trapecioinv@gmail.com>
2024-07-27 15:35:22 -0300

    ADD: Domain basic data

---
+++ src/main/java/com/example/trapecioinv/domain/dto/PersonDto.java
@@ -0,0 +1,11 @@
+package com.example.trapecioinv.domain.dto;
+
+import lombok.Data;
+
+@Data
+public class PersonDto {
+    private String name;
+    private String lastName;
+    private String email;
+    private int age;
+}
---
+++ src/main/java/com/example/trapecioinv/domain/entities/Person.java
@@ -0,0 +1,11 @@
+package com.example.trapecioinv.domain.entities;
+
+import lombok.Data;
+
+@Data
+public class Person {
+    private String name;
+    private String lastName;
+    private String email;
+    private int age;
+}
